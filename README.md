de560d773dc4a652954820dadbe0fc933ef4b262
Adelina-Sabrina Gherman <37504469+Sabrinaadelina@users.noreply.github.com>
2018-09-02 13:39:12 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,19 +2,22 @@
 
 ## Table of Contents
 
+* [Memory Game](#memoryGame)
 * [Instructions](#instructions)
-* [Contributing](#contributing)
+* [Challenge](#challenge)
+* [Contribuing](#contribuig)
+
+## Memory Game
+This is a simple game built to test a users memory. There's a deck of cards with different icons. 
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+The game is started once the user click the first card, in order to finish the game, all 16 cards must be matched.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Challenge
+Match cards in less time with less moves.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## Contribuing
+https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css
+https://fonts.googleapis.com/css?family=Coda
--- js/app.js
+++ js/app.js
@@ -2,32 +2,230 @@
  * Create a list that holds all of your cards
  */
 
-
+const allCards = ["fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-diamond", "fa fa-diamond",
+				  "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt","fa fa-cube", "fa fa-cube",
+				  "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+const cardsContainer = document.querySelector(".deck");
+let shuffleAllCards = shuffle(allCards);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+let openedCards = [];
+let matchedCards = [];
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
 
-    return array;
+function initGame() {
+
+
+	for (let i = 0; i < allCards.length; i++) {
+		const card = document.createElement("li");
+		card.classList.add("card");
+		card.innerHTML = `<i class="${allCards[i]}"> </i> `;
+		shuffleAllCards.forEach(function (addCards) {
+			cardsContainer.appendChild(card);
+		});
+
+
+		// call function shuffle
+
+		click(card);
+
+
+	}
+
+
+}
+
+//set up the event listener for a card. If a card is clicked:
+function click(card) {
+
+    
+	card.addEventListener("click", function () {
+		if (isFirstClick) {
+			isFirstClick = true;
+		}
+
+		const currentCard = this;
+		const previousCard = openedCards[0];
+		
+
+		if (openedCards.length === 1) {
+
+
+			card.classList.add("open", "show", "disabled");
+			openedCards.push(this);
+
+			compare(currentCard, previousCard);
+
+		} else {
+
+			currentCard.classList.add("open", "show", "disabled");
+			openedCards.push(this);
+
+		}
+
+	
+	}, 400);
+	
+	
+
+}
+
+//if the cards do match, lock the cards in the open position
+function compare(currentCard, previousCard) {
+
+	if (currentCard.innerHTML === previousCard.innerHTML) {
+
+		currentCard.classList.add("match");
+		previousCard.classList.add("match");
+
+		matchedCards.push(currentCard, previousCard);
+
+		openedCards = [];
+
+		//endGame();
+		displayMessage();
+
+	} else {
+		//remove the cards from the list and hide the card's symbol
+		setTimeout(function () {
+			currentCard.classList.remove("open", "show", "disabled");
+
+			previousCard.classList.remove("open", "show", "disabled");
+			openedCards = [];
+		}, 600);
+
+
+	}
+	moveCounter();
+	
 }
 
 
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
+movesContainer.innerHTML = 0;
+//increment the move counter and display it on the page
+function moveCounter() {
+	moves++;
+	movesContainer.innerHTML = moves;
+	rating();
+
+}
+//star rating
+const starsContainer = document.querySelector(".stars");
+const star = `<li><i class="fa fa-star"></i></li>`;
+starsContainer.innerHTML = star + star + star;
+
+function rating() {
+
+	if (moves < 12) {
+		starsContainer.innerHTML = star + star + star;
+	} else if (moves < 15) {
+		starsContainer.innerHTML = star + star;
+	} else {
+		starsContainer.innerHTML = star;
+	}
+}
+const timerContainer = document.querySelector(".timer");
+let liveTimer,
+	totalSeconds = 0;
+timerContainer.innerHTML = totalSeconds + "s";
+
+function startTimer() {
+	liveTimer = setInterval(function () {
+		totalSeconds++;
+		//display time
+		timerContainer.innerHTML = totalSeconds + "s";
+	}, 1000);
+}
+
+function stopTimer() {
+	clearInterval(liveTimer);
+}
+
+let isFirstClick = false;
+//restart button
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", function () {
+
+	cardsContainer.innerHTML = "";
+
+	initGame();
+
+	matchedCards = [];
+	moves = 0;
+	movesContainer.innerHTML = moves;
+	totalSeconds = 0;
+	timerContainer = 0;
+
+
+});
+
+function reset() {
+	matchedCards = [];
+	moves = 0;
+	movesContainer.innerHTML = moves;
+	starsContainer.innerHTML = star + star + star;
+	totalSeconds = 0;
+	timerContainer.innerHTML = 0;
+	restartButton = true;
+	isFirstClick = true;
+
+}
+const modal = document.querySelector('.modal');
+
+function displayMessage() {
+	if (matchedCards.length == allCards.length) {
+		clearInterval(liveTimer);
+		modal.innerHTML = ` <h2>Congrats! <br/> You matched all the cards with 
+            ${moves} moves, <br/>
+            in ${totalSeconds} seconds <br/> and with the following number of stars: 
+            ${starsContainer.innerHTML}`
+		modal.classList.add('modal');
+		modal.style.display = "block";
+
+		stopTimer();
+		clearInterval();
+
+	}
+}
+//restart game by clicking on restart icon
+const replay = document.querySelector('.restart');
+replay.addEventListener('click', function () {
+	window.location.reload();
+});
+
+//reload function for clicking on Yes button within modal
+function reload() {
+	window.location.reload();
+}
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue, randomIndex;
+
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+startTimer();
+initGame();
+
+
 /*
- * set up the event listener for a card. If a card is clicked:
+ * 
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
